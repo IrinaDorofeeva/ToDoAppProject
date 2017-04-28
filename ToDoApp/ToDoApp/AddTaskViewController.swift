8be1a64704f3b00ae756d98d11cb8dced964a15a
Irina Dorofeeva <idorofeeva@gmail.com>
2017-04-28 13:17:40 -0700

    first commit

---
+++ ToDoApp/ToDoApp/AddTaskViewController.swift
@@ -0,0 +1,32 @@
+//
+//  AddTaskViewController.swift
+//  ToDoApp
+//
+//  Created by Mac on 4/28/17.
+//  Copyright © 2017 Mac. All rights reserved.
+//
+
+import UIKit
+
+class AddTaskViewController: UIViewController {
+
+    
+    @IBOutlet weak var addTask: UITextField!
+    
+    @IBOutlet weak var isImportantSwitch: UISwitch!
+    
+    
+    @IBOutlet weak var addTaskButton: UIButton!
+    
+    
+    @IBAction func addTaskAction(_ sender: Any) {
+    }
+    
+    override func viewDidLoad() {
+        
+
+        // Do any additional setup after loading the view.
+    }
+
+   
+}
